e64d89dc56be7531fc78935076c15d8e1bd3d8ba
example-id123 <167778296+example-id123@users.noreply.github.com>
2024-04-22 22:30:59 +0530

    Update Test_002.java

--- src/test/java/com/T1/Test_002.java
+++ src/test/java/com/T1/Test_002.java
@@ -3,7 +3,8 @@ package com.T1;
 public class Test_002 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+			System.out.println("test 002");
+			System.out.println("New implementation");
 
 	}
 
